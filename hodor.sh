5b8ba9e76557560275ba7f39176840ec2ac3c414
romainzap <romain.zap@gmail.com>
2022-12-13 06:01:33 -0500

    Exo hodor

--- hodor.sh
+++ hodor.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+
 toto=$(df -i / | tail -n 1 | cut -d ' ' -f 12 | sed 's/%//')
-if ($toto -gt 20 )
+echo $toto
+if [ $toto -gt 20 ]
 then
 echo " C'est au dessus de 20"
-tar -czvf
+tar -czvf  /var/log/exercice/archives/fichier_archivés /var/log/exercice/recent_logs
+echo "HODOOOOOR">> /dev/pts/0
 else
 echo " C'est OK "
 fi
-echo $toto
+

abb93850ed61dcd59480473e71b501f3f2c04104
romainzap <romain.zap@gmail.com>
2022-12-13 04:14:12 -0500

    EXO_Linux

--- hodor.sh
+++ hodor.sh
@@ -1,1 +1,9 @@
- :(head((cut(df -i| cut -d " " -f 5))))i
+toto=$(df -i / | tail -n 1 | cut -d ' ' -f 12 | sed 's/%//')
+if ($toto -gt 20 )
+then
+echo " C'est au dessus de 20"
+tar -czvf
+else
+echo " C'est OK "
+fi
+echo $toto
